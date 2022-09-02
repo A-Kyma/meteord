ba0655a46377957b94e0f763fdff89dace9226d0
arnaud <adoyen@perso.be>
2022-09-02 18:01:07 +0200

    Add mup in meteord docker image

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -4,6 +4,7 @@ bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
 #bash $METEORD_DIR/lib/install_phantomjs.sh
 #bash $METEORD_DIR/lib/install_forever.sh
+bash $METEORD_DIR/lib/install_mup.sh
 bash $METEORD_DIR/lib/install_imagemagick.sh
 bash $METEORD_DIR/lib/install_wkhtmltopdf.sh
 bash $METEORD_DIR/lib/install_qpdf.sh
