8d9de5a7bd2ffae2d387c09396a0711960eab4ce
arnaud <adoyen@perso.be>
2022-09-05 18:09:07 +0200

    Update docker meteord

--- base/scripts/init_meteor_env.sh
+++ base/scripts/init_meteor_env.sh
@@ -1,11 +1,17 @@
 set -e
+export METEORD_DIR="/opt/meteord"
+export NODE_VERSION="14.19.3"
 
 bash $METEORD_DIR/lib/install_base.sh
+bash $METEORD_DIR/lib/install_procps.sh
 bash $METEORD_DIR/lib/install_node.sh
 #bash $METEORD_DIR/lib/install_phantomjs.sh
 #bash $METEORD_DIR/lib/install_forever.sh
-#bash $METEORD_DIR/lib/install_mup.sh
+bash $METEORD_DIR/lib/install_mup.sh
+bash $METEORD_DIR/lib/install_meteor.sh
 bash $METEORD_DIR/lib/install_imagemagick.sh
 bash $METEORD_DIR/lib/install_wkhtmltopdf.sh
 bash $METEORD_DIR/lib/install_qpdf.sh
 bash $METEORD_DIR/lib/cleanup.sh
+
+useradd meteor
--- base/scripts/lib/install_mup.sh
+++ base/scripts/lib/install_mup.sh
@@ -1,1 +1,3 @@
 npm install --global mup
+
+export PATH=$PATH:/opt/nodejs/bin/
